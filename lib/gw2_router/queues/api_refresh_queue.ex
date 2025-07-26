b9700c66f8ebe8f0fb59c71d7157d949553b6673
Magnus Wulf <magnus@wulf.contact>
2025-07-26 21:39:22 +0200

    admin page with waypoint refresh from api

--- lib/gw2_router/queues/api_refresh_queue.ex
+++ lib/gw2_router/queues/api_refresh_queue.ex
@@ -1,9 +1,36 @@
 defmodule Gw2Router.APIRefreshQueue do
-  use Oban.Worker, queue: :data_refresh, max_attempts: 2
+  alias Gw2Router.Waypoint
+  use Oban.Worker, queue: :data_refresh, max_attempts: 1
+
   require Logger
 
+  @endpoint "https://api.guildwars2.com/v2/continents/1/floors?ids=all"
+  # @endpoint "https://api.guildwars2.com/v2/continents/"
+
   @impl Oban.Worker
-  def perform(%Oban.Job{args: args}) do
-    IO.inspect(args)
+  def perform(%Oban.Job{args: _args}) do
+    Phoenix.PubSub.broadcast(Gw2Router.PubSub, "admin_page", {:job_started})
+
+    with {wp_count, _} <- get_gw2_api_data() do
+      Phoenix.PubSub.broadcast(Gw2Router.PubSub, "admin_page", {:job_done, wp_count})
+      :ok
+    else
+      {:error, reason} ->
+        Phoenix.PubSub.broadcast(Gw2Router.PubSub, "admin_page", {:job_failed})
+        {:error, "DB Error"}
+    end
+  end
+
+  defp get_gw2_api_data() do
+    with {:ok, %Req.Response{body: body}} <- Req.get(@endpoint) do
+      full_wp_changeset = Gw2Router.Waypoint.build_insert_list(body)
+      # File.write("test2.json", Jason.encode!(full_wp_changeset))
+      # IO.inspect(full_wp_changeset)
+
+      Gw2Router.Repo.insert_all(Gw2Router.Waypoint, full_wp_changeset,
+        on_conflict: {:replace_all_except, [:id]},
+        conflict_target: :poi_id
+      )
+    end
   end
 end
--- lib/gw2_router_web/live/admin_live.ex
+++ lib/gw2_router_web/live/admin_live.ex
@@ -1,7 +1,33 @@
 defmodule Gw2RouterWeb.AdminLive do
   use Gw2RouterWeb, :live_view
 
+  @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    Phoenix.PubSub.subscribe(Gw2Router.PubSub, "admin_page")
+    {:ok, socket |> assign(:result, "")}
+  end
+
+  @impl true
+  def handle_event("refresh_api_data", _params, socket) do
+    # Gw2Router.APIRefreshQueu
+    Gw2Router.APIRefreshQueue.new(%{})
+    |> Oban.insert()
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({:job_started}, socket) do
+    {:noreply, socket |> assign(:result, "In Progress")}
+  end
+
+  @impl true
+  def handle_info({:job_done, result}, socket) do
+    {:noreply, socket |> assign(:result, "Job is done with #{result} waypoints")}
+  end
+
+  @impl true
+  def handle_info({:job_failed}, socket) do
+    {:noreply, socket |> assign(:result, "Job Failed")}
   end
 end
